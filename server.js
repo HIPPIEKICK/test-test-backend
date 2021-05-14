6ebf7fa99fb442a35b144130428b26bd3de4e8ed
Matilda Arvidsson <matilda.j.arvidsson@gmail.com>
2021-05-14 17:35:44 +0200

    test

--- server.js
+++ server.js
@@ -6,8 +6,11 @@ import cors from 'cors'
 // overridden when starting the server. For example:
 //
 //   PORT=9000 npm start
-const port = process.env.PORT || 8080
+const port = process.env.PORT || 9800
 const app = express()
+
+const dotenv = require('dotenv')
+dotenv.config()
 
 // Add middlewares to enable cors and json body parsing
 app.use(cors())
@@ -17,6 +20,37 @@ app.use(bodyParser.json())
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
+
+const fetch = require("node-fetch");
+
+// const API_KEY = "YOUR_API_KEY";
+// console.log(new Date(Date.now()))
+const data = {
+  isLocked: true,
+  roomNamePrefix: "/matilda",
+  roomNamePattern: "human-short",
+  startDate: new Date(Date.now()),
+  endDate: "3022-05-15T13:56:00.000Z",
+  fields: ["hostRoomUrl"],
+};
+
+function getResponse() {
+    return fetch("https://api.whereby.dev/v1/meetings", {
+        method: "POST",
+        headers: {
+            Authorization: `Bearer ${process.env.API_KEY}`,
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+    });
+}
+
+getResponse().then(async res => {
+    console.log("Status code:", res.status);
+    const data = await res.json();
+    console.log("Room URL:", data.roomUrl);
+    console.log("Host room URL:", data.hostRoomUrl);
+});
 
 // Start the server
 app.listen(port, () => {
